95c2a49d3e110094d53eb38b15ffb9ab035db3de
Bonnie <bonniec15@nycstudents.net>
2022-10-30 10:33:57 -0400

    parsing of numbers from expression

--- src/Main.java
+++ src/Main.java
@@ -11,22 +11,25 @@ public class Main {
         String sign;
         if(expression.indexOf("*") != -1) {         //checks to see if sign is multiplication
             sign = "*";
-        }
-        if(expression.indexOf("/") != -1) {         //checks to see if sign is division
+        }else if(expression.indexOf("/") != -1) {         //checks to see if sign is division
             sign = "/";
-        }
-        if(expression.indexOf("+") != -1) {         //checks to see if sign is addition
+        }else if(expression.indexOf("+") != -1) {         //checks to see if sign is addition
             sign = "+";
-        }
-        if(expression.indexOf("-") != -1) {         //checks to see if sign is subtraction
+        }else if(expression.indexOf("-") != -1) {         //checks to see if sign is subtraction
             sign = "-";
-        }
-        if(expression.indexOf("%") != -1) {         //checks to see if sign is remainder
+        }else if(expression.indexOf("%") != -1) {         //checks to see if sign is remainder
             sign = "%";
-        }
-        if(expression.indexOf("^") != -1) {         //checks to see if sign is to the power of
+        }else {                                           //checks to see if sign is to the power of
             sign = "^";
         }
+
+        //parsing numbers from expression
+        double num1 = Double.parseDouble(expression.substring(0, (expression.indexOf(sign)-1)));
+        double num2 = Double.parseDouble(expression.substring(expression.indexOf(sign) +1));
+
+        //creating object calculator
+        Calculator calculate = new Calculator(num1, num2, sign);
+
 
 
 
